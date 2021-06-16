d83b4e1480b5b1757226f9dc65bf58e1ba244290
Pankaj Wakchaure <pankaj.corp@gmail.com>
2021-06-16 08:18:59 +0000

    callbacks 3

--- src/node/callbacks-quiz.js
+++ src/node/callbacks-quiz.js
@@ -38,18 +38,52 @@
 // });
 
 // q2a1
-const fs = require("fs");
+// const fs = require("fs");
+// const path = require("path");
+// const filePath = path.join(__dirname, "./files") + "/demofile.txt";
+// function readFileThenDo(next) {
+//   fs.readFile(filePath, "utf8", (err, data) => {
+//     next(err, data);
+//   });
+// }
+// readFileThenDo((err, data) => {
+//   if (err) {
+//     throw Error(`readFileThenDo error: ${err}`);
+//   }
+//   console.log(data);
+// });
+
+
+// q3
+// Instead of passing it up the stack throw it instead and try to catch it later on.
+// const fs = require("fs");
+// const path = require("path");
+// const filePath = path.join(__dirname, "./files") + "/demofile.txt";
+// function readFileThenDo(next) {
+//   fs.readFile(filePath, "utf8", (err, data) => {
+//     if (err) throw err;
+//     next(data);
+//   });
+// }
+// // Hint use try..catch
+// readFileThenDo(data => {
+//   console.log(data);
+// });
+
+// q3a1
+// Or if the error is serious, you can throw the error as soon as you see it.
+// try..catch desn't work as you expect with callbacks, it only really works with synchronous code.
+// By the time the callback throws the error we have moved on from the try..catch, the throw happens in the root scope and will just cause the program to exit.
 const path = require("path");
 const filePath = path.join(__dirname, "./files") + "/demofile.txt";
-console.log(filePath);
 function readFileThenDo(next) {
   fs.readFile(filePath, "utf8", (err, data) => {
-    next(err, data);
+    if (err) throw err;
+    next(null, data);
   });
 }
-readFileThenDo((err, data) => {
-  if (err) {
-    throw Error(`readFileThenDo error: ${err}`);
-  }
-  console.log(data);
-});
+try {
+  readFileThenDo((_, data) => console.log(data));
+} catch (err) {
+  console.error(err);
+}
